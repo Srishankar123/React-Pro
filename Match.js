d82e2054e965035724ffa9d2a9a72839306bcf3f
Srishankar123 <76104274+Srishankar123@users.noreply.github.com>
2022-12-07 12:39:49 +0530

    Add files via upload

---
+++ Match.js
@@ -0,0 +1,11 @@
+
+function Match(){
+
+    const bowl= (speed) =>{
+            alert(speed);
+    }
+
+    return <button onClick={() =>bowl("got a wicket")}>TAKE THE SHOT</button>
+}
+
+export default Match;
